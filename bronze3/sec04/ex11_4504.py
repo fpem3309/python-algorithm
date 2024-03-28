efc83cfe8287e936082b91a01beb52d073437f57
fpem3309 <fpem3309@gmail.com>
2024-03-28 14:51:44 +0900

    python bronze3 끝

---
+++ bronze3/sec04/ex11_4504.py
@@ -0,0 +1,8 @@
+n = int(input())
+while True:
+    num = int(input())
+    if num == 0: break
+    if num % n == 0:
+        print("{} is a multiple of {}".format(num, n))
+    else:
+        print("{} is NOT a multiple of {}".format(num, n))
---
+++ bronze3/sec04/ex12_10178.py
@@ -0,0 +1,4 @@
+t = int(input())
+for i in range(t):
+    c, v, = map(int, input().split())
+    print("You get {} piece(s) and your dad gets {} piece(s).".format(c // v, c % v))
